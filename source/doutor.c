23384c88d6a98cad38324750c2f38a157e523f75
Unknown <overskilers@gmail.com>
2017-12-08 00:21:36 +0000

    Revert "Já faz o contador de pacientes tratados e triados e o tempo médio(FALTAM OS SEMAFOROS E O TEMPO TOTAL)"
    This reverts commit 5baab3a1529d9ee4e18d3f1f58a79a59a3d2a363.

--- source/doutor.c
+++ source/doutor.c
@@ -15,29 +15,21 @@ void trataPaciente_tempDoctor();
 void* createTempDoctor();
 
 int requestDoctor = 0;
-int n_pacientes_tratados = 0;
-int tempo_atendimento=0;
-int tempo_atendimento_med;
 pthread_t temp_doctor_thread;
 
 void* createDoctors(){
+
   //Cria os doutores iniciais
   for(int i=0; i<globalVars.DOCTORS; i++){
     if(fork() == 0){
       trataPaciente();
       exit(0);
     }
-    else if(fork() != 0){
-      printf("Erro na criacao do processo\n");
-    }
   }
   printf("Thread [%ld] criou doutores iniciais.\n", pthread_self());
 
   //Thread que vai criar o doutor temporario, se for preciso
-  if(pthread_create(&temp_doctor_thread, 0, createTempDoctor, NULL)==0){
-    continue;
-  }
-  else(printf("Erro na criacao do doutor temporario\n"));
+  pthread_create(&temp_doctor_thread, 0, createTempDoctor, NULL);
 
   //Quando um acabar, começa outro
   while(1){
@@ -47,9 +39,6 @@ void* createDoctors(){
       trataPaciente();
       exit(0);
     }
-    else if(fork()!=0){
-      printf("Erro na criaçao de procesos");
-    }
   }
 
   pthread_exit(NULL);
@@ -58,18 +47,13 @@ void* createDoctors(){
 void trataPaciente(){
   Paciente paciente;
   struct msqid_ds* info_mq = malloc(sizeof(struct msqid_ds));
-  time_t start_time = time(NULL), end_time,start_atender,end_atender;
+  time_t start_time = time(NULL), end_time;
 
   printf("Doutor [%d] começou o seu turno.\n", getpid());
 
   while((end_time-start_time) < globalVars.SHIFT_LENGTH){
     //Verifica o estado da message queue
-    if(sem_wait(&globalVars.semMQ)==0){
-      continue;
-    }
-    else{
-      printf("Erro no semaforo\n");
-    }
+    sem_wait(&globalVars.semMQ);
 
     //Verifica se ja foi pedido doutor adicional
     if(requestDoctor == 0){ //Ainda n foi pedido, pode ser pedido
@@ -80,47 +64,24 @@ void trataPaciente(){
         //Atingiu mais de 100% de lotaçao, faz signal a thread para fazer mais um processo (por fazer)
         printf("Requesting temporary doctor\n");
         requestDoctor = 1;
-        if(pthread_mutex_lock(&globalVars.mutex_doctor) ==0){
-          continue;
-        }
-        else{
-          printf("Erro no mutex_lock\n");
-        }
-        if(pthread_cond_signal(&globalVars.cond_var_doctor)==0){
-          continue;
-        }
-        else{
-          printf("Erro na espera da condicao\n");
-        }
-        if(pthread_mutex_unlock(&globalVars.mutex_doctor)==0){
-          continue;
-        }
-        else(printf("Erro no mutex_unlock\n"));
+        pthread_mutex_lock(&globalVars.mutex_doctor);
+        pthread_cond_signal(&globalVars.cond_var_doctor);
+        pthread_mutex_unlock(&globalVars.mutex_doctor);
       }
     }
     else{
       //Já foi pedido, n pode ser pedido outra vez
     }
 
-    if(sem_post(&globalVars.semMQ)==0){
-      continue;
-    }
-    else{
-      printf("Erro no semaforo\n");
-    }
+    sem_post(&globalVars.semMQ);
 
     //Recebe paciente da queue
     if(msgrcv(globalVars.mq_id_doctor, &paciente, sizeof(Paciente)-sizeof(long), MTYPE, IPC_NOWAIT) > 0){
       printf("Doutor [%d] recebeu paciente %s\n", getpid(), paciente.nome);
-      start_atender = time(NULL);
       //Escreve nas estatisticas (por fazer)
 
       //Espera tempo de atendimento
       usleep(paciente.atend_time*1000); //Converte para milisegundos
-      end_atender = time(NULL);
-      tempo_atendimento += end_atender - start_atender;
-      n_pacientes_tratados +=1;
-      tempo_antes_med = tempo_atendimento/n_pacientes_tratados;
       printf("Doutor [%d] atendeu paciente %s\n", getpid(), paciente.nome);
     }
     else{
@@ -135,56 +96,28 @@ void* createTempDoctor(){
   pid_t temp_doctor;
   //Vai esperar que seja acordada para criar mais um processo temporario
   while(1){
-    if(pthread_mutex_lock(&globalVars.mutex_doctor) ==0){
-      continue;
-    }
-    else{
-      printf("Erro no mutex_lock\n");
-    }
+    pthread_mutex_lock(&globalVars.mutex_doctor);
 
     while(!requestDoctor){
-      if(pthread_cond_wait(&globalVars.cond_var_doctor, &globalVars.mutex_doctor)==0){
-        continue;
-      }
-      else{
-        print("Erro na espera da condicao");
-      }
+      pthread_cond_wait(&globalVars.cond_var_doctor, &globalVars.mutex_doctor);
     }
 
     //Doutor temporario pedido
     printf("\n\nTemporary doctor requested\n\n");
-    if(pthread_mutex_unlock(&globalVars.mutex_doctor)==0){
-      continue;
-    }
-    else{
-      printf("Erro no mutex_unlock\n");
-    }
+    pthread_mutex_unlock(&globalVars.mutex_doctor);
 
     //Cria um doutor
     if((temp_doctor = fork()) == 0){
       trataPaciente_tempDoctor();
       exit(0);
     }
-    else{
-      printf("Erro na criacao do doutor temporario\n");
-    }
 
     //Espera que ele acabe
     waitpid(temp_doctor, NULL, WNOHANG);
     //Acabou, volta a colocar a condiçao a 0
-    if(pthread_mutex_lock(&globalVars.mutex_doctor)==0){
-      continue;
-    }
-    else{
-      printf("Erro no mutex_lock\n");
-    }
+    pthread_mutex_lock(&globalVars.mutex_doctor);
     requestDoctor = 0;
-    if(pthread_mutex_unlock(&globalVars.mutex_doctor)==0){
-      continue;
-    }
-    else{
-      printf("Erro no mutex_unlock\n");
-    }
+    pthread_mutex_unlock(&globalVars.mutex_doctor);
   }
   pthread_exit(NULL);
 }
@@ -197,24 +130,13 @@ void trataPaciente_tempDoctor(){
 
   while(1){
     //Verifica o estado da message queue
-    if(sem_wait(&globalVars.semMQ)==0){
-      continue;
-    }
-    else{
-      printf("Erro no semaforo\n");
-    }
-
+    sem_wait(&globalVars.semMQ);
     msgctl(globalVars.mq_id_doctor, IPC_STAT, info_mq);
     if(info_mq->msg_qnum < ((globalVars.MQ_MAX*80*100)/100.0)){
       //Voltou a menos de 80% de lotação, acaba este processo nesta iteraçao
       check_exit = 1;
     }
-    if(sem_post(&globalVars.semMQ)==0){
-      continue;
-    }
-    else{
-      printf("Erro no semaforo\n");
-    }
+    sem_post(&globalVars.semMQ);
 
     //Recebe paciente da queue
     msgrcv(globalVars.mq_id_doctor, &paciente, sizeof(Paciente)-sizeof(long), MTYPE, 0);
--- source/triagem.c
+++ source/triagem.c
@@ -12,23 +12,16 @@
 //Operaçoes triagem
 
 Globals globalVars;
-int n_pacientes_triados = 0;
-int tempo_antes = 0;
-int tempo_antes_med;
 
 void* triaPaciente(void* t){
   Paciente paciente;
-  time_t start_time = time(NULL), end_time;
+
   while(1){
     msgrcv(globalVars.mq_id_thread, &paciente, sizeof(Paciente)-sizeof(long), MTYPE, 0);
     printf("Thread [%ld] recebeu paciente %s\n", pthread_self(), paciente.nome);
     //Escreve as estatisticas em memoria partilhada (por fazer)
     //Espera pelo tempo de triagem
     usleep(paciente.triage_time);
-    n_pacientes_triados += 1;
-    end_time = time(NULL);
-    tempo_antes += start_time - end_time;
-    tempo_antes_med = tempo_antes/n_pacientes_triados;
     msgsnd(globalVars.mq_id_doctor, &paciente, sizeof(Paciente)-sizeof(long), 0);
     printf("Thread [%ld] enviou paciente %s\n", pthread_self(), paciente.nome);
   }
